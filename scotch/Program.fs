d741fd9a2914dd97a3207fb1917013a8053dec76
Martin Leech <leech.martin@gmail.com>
2015-10-06 21:45:18 +0800

    Split RecordingHandler into own file

--- scotch/Program.fs
+++ scotch/Program.fs
@@ -1,8 +1,25 @@
-﻿open System.IO
+﻿open Fleece
+open Fleece.Operators
+open System
+open System.IO
+open System.Json
+open System.Net
+open System.Net.Http
+open System.Threading.Tasks
 open Scotch
-open Fleece
+
+let makeHttpCallAsync () =
+    async {
+        let clientHandler = new HttpClientHandler()
+        use recordingHandler = new Scotch.RecordingHandler("C:/Users/Martin/dev/testing123.json", clientHandler)
+        let httpClient = new HttpClient(recordingHandler)
+        httpClient.GetAsync("http://jsonplaceholder.typicode.com/posts/1") |> Async.AwaitTask |> ignore
+        httpClient.GetAsync("http://jsonplaceholder.typicode.com/posts/2") |> Async.AwaitTask |> ignore
+        httpClient.GetAsync("http://jsonplaceholder.typicode.com/posts/3") |> Async.AwaitTask |> ignore
+        return ()
+    }
 
 [<EntryPoint>]
 let main argv =
-    Scotch.makeHttpCallAsync () |> Async.RunSynchronously
+    makeHttpCallAsync () |> Async.RunSynchronously
     0
---
+++ scotch/RecordingHandler.fs
@@ -0,0 +1,39 @@
+﻿namespace Scotch
+
+open System
+open System.Net
+open System.Net.Http
+open System.Threading.Tasks
+open Scotch
+
+type RecordingHandler(filePath:string, innerHandler:HttpMessageHandler) =
+    inherit DelegatingHandler(innerHandler)
+
+    let mutable interactions = []
+    let mutable tasks = []
+
+    override handler.SendAsync (request:HttpRequestMessage, cancellationToken:Threading.CancellationToken) =
+        let baseResult = base.SendAsync(request, cancellationToken)
+        let workflow = async {
+            let! response = baseResult |> Async.AwaitTask
+            let! interactionRequest = convertRequestAsync request
+            let! interactionResponse = convertResponseAsync response
+            let httpInteraction =
+                {Request = interactionRequest
+                 Response = interactionResponse
+                 RecordedAt = DateTimeOffset.Now}
+            interactions <- List.Cons(httpInteraction, interactions)
+            return response
+        }
+
+        let task = Async.StartAsTask workflow
+        tasks <- List.Cons(task, tasks)
+        task
+
+    override handler.Dispose (disposing:bool) =
+        if disposing then
+            Task.WaitAll [| for t in tasks -> t :> Task |]
+            persistCassetteToFile (filePath, List.rev interactions)
+
+        base.Dispose(disposing)
+
